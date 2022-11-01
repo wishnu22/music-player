e87084ad1b2153a58d06221e00edc14773b9cf0e
Wishnu22 <vishnuunnikrishnan594@gmail.com>
2022-11-01 15:57:51 +0530

    committed

--- script.js
+++ script.js
@@ -22,29 +22,24 @@ let updateTimer;
 
 const music_list = [
     {
-        img : 'images/stay.png',
-        name : 'Stay',
-        artist : 'The Kid LAROI, Justin Bieber',
-        music : 'music/stay.mp3'
+        img : 'images/vibi.jpeg',
+        name : 'idukki gold',
+        artist : 'vipin home',
+        music : 'music/idukki.mpeg'
     },
     {
-        img : 'images/fallingdown.jpg',
-        name : 'Falling Down',
-        artist : 'Wid Cards',
-        music : 'music/fallingdown.mp3'
+        img : 'images/jithinmarr.jpeg',
+        name : 'Happy days',
+        artist : 'jithins wedding',
+        music : 'music/ayyayyo.mp3'
     },
     {
-        img : 'images/faded.png',
-        name : 'Faded',
-        artist : 'Alan Walker',
-        music : 'music/Faded.mp3'
+        img : 'images/umesh.jpeg',
+        name : 'vidachollan',
+        artist : 'trip',
+        music : 'music/vida.mp3'
     },
-    {
-        img : 'images/ratherbe.jpg',
-        name : 'Rather Be',
-        artist : 'Clean Bandit',
-        music : 'music/Rather Be.mp3'
-    }
+   
 ];
 
 loadTrack(track_index);
